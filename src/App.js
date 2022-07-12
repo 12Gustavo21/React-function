eb573bcf976b0ab7c608042c2e470e82769a21bc
Gustavo <104770923+12Gustavo21@users.noreply.github.com>
2022-07-12 10:11:48 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <section>
+      <h1>UwU</h1>
+      <h2>-O-</h2>
+      <p>👻👻👻</p>
+    </section>
+  );
+}
+export default App;
